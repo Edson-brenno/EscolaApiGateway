0732873f8940c3aaec129e44b85dd7e7e50f36a7
brenno <brennorebeiro@gmail.com>
2024-08-02 01:40:29 -0300

    Add config

--- src/main/java/com/spring/escolaapigateway/EscolaApiGatewayApplication.java
+++ src/main/java/com/spring/escolaapigateway/EscolaApiGatewayApplication.java
@@ -1,9 +1,12 @@
 package com.spring.escolaapigateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.spring.Config")
 public class EscolaApiGatewayApplication {
 
     public static void main(String[] args) {
