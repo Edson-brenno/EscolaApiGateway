d127b04b8a7e8a68d223b5ec430aa4293b9344a5
brenno <brennorebeiro@gmail.com>
2024-08-02 00:54:07 -0300

    Start project

---
+++ src/main/java/com/spring/escolaapigateway/EscolaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.spring.escolaapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscolaApiGatewayApplication.class, args);
+    }
+
+}
